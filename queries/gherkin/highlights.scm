412ec86d3de9923a644b5d8db3ce4446b98aee4f
shoebox <169844+github@users.noreply.github.com>
2026-04-07 15:28:42 +0100

    feat(lang): add gherkin tree-sitter queries

---
+++ queries/gherkin/highlights.scm
@@ -0,0 +1,81 @@
+[
+  (background_kw)
+  (examples_kw)
+  (feature_kw)
+  (rule_kw)
+  (scenario_kw)
+  (scenario_outline_kw)
+] @keyword
+
+(given_group
+  [
+    (given_step
+      (given_line
+        (given_kw) @keyword.import))
+    (asterisk_step
+      (asterisk_line
+        "* " @keyword.import))
+    (and_step
+      (and_line
+        (and_kw) @keyword.import))
+    (but_step
+      (but_line
+        (but_kw) @keyword.import))
+  ])
+
+(when_group
+  [
+    (when_step
+      (when_line
+        (when_kw) @function))
+    (asterisk_step
+      (asterisk_line
+        "* " @function))
+    (and_step
+      (and_line
+        (and_kw) @function))
+    (but_step
+      (but_line
+        (but_kw) @function))
+  ])
+
+(then_group
+  [
+    (then_step
+      (then_line
+        (then_kw) @type))
+    (asterisk_step
+      (asterisk_line
+        "* " @type))
+    (and_step
+      (and_line
+        (and_kw) @type))
+    (but_step
+      (but_line
+        (but_kw) @type))
+  ])
+
+(step_param) @variable.parameter
+
+(tag) @type
+
+(table_head_row
+  (table_col
+    (table_cell) @markup.heading))
+
+":" @punctuation.delimiter
+
+"|" @punctuation.special
+
+(doc_string) @string.documentation
+(media_type) @type
+
+"language" @property
+
+(language_name) @string
+(invalid_language_name) @comment.error
+
+[
+  (comment)
+  (language)
+] @comment
