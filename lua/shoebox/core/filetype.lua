d56719cf1cbddc7af62fb224864598a932dc4077
shoebox <169844+github@users.noreply.github.com>
2026-04-07 15:26:33 +0100

    feat(core): add ansible filetype detection and fix gherkin pattern

--- lua/shoebox/core/filetype.lua
+++ lua/shoebox/core/filetype.lua
@@ -1,5 +1,3 @@
-vim.g.do_filetype_lua = 1
-
 vim.filetype.add({
   extension = {
     ["http"] = "http",
@@ -17,6 +15,30 @@ vim.filetype.add({
 
     ["haproxy.cfg"] = "haproxy",
     [".*/config.d/.*%.cfg"] = "haproxy",
-    ["*.feature"] = "gherkin",
+    [".*%.feature"] = "gherkin",
+
+    -- Ansible (path-based detection)
+    -- Note: Lua patterns don't support `?` quantifier, so we need separate patterns for .yml and .yaml
+    -- Using `.*` without trailing `/` allows matching both absolute and relative paths
+    [".*playbooks/.*%.yml"] = "yaml.ansible",
+    [".*playbooks/.*%.yaml"] = "yaml.ansible",
+    [".*playbook/.*%.yml"] = "yaml.ansible",
+    [".*playbook/.*%.yaml"] = "yaml.ansible",
+    [".*roles/.*/tasks/.*%.yml"] = "yaml.ansible",
+    [".*roles/.*/tasks/.*%.yaml"] = "yaml.ansible",
+    [".*roles/.*/handlers/.*%.yml"] = "yaml.ansible",
+    [".*roles/.*/handlers/.*%.yaml"] = "yaml.ansible",
+    [".*roles/.*/defaults/.*%.yml"] = "yaml.ansible",
+    [".*roles/.*/defaults/.*%.yaml"] = "yaml.ansible",
+    [".*roles/.*/vars/.*%.yml"] = "yaml.ansible",
+    [".*roles/.*/vars/.*%.yaml"] = "yaml.ansible",
+    [".*roles/.*/meta/.*%.yml"] = "yaml.ansible",
+    [".*roles/.*/meta/.*%.yaml"] = "yaml.ansible",
+    [".*group_vars/.*%.yml"] = "yaml.ansible",
+    [".*group_vars/.*%.yaml"] = "yaml.ansible",
+    [".*host_vars/.*%.yml"] = "yaml.ansible",
+    [".*host_vars/.*%.yaml"] = "yaml.ansible",
+    [".*inventory/.*%.yml"] = "yaml.ansible",
+    [".*inventory/.*%.yaml"] = "yaml.ansible",
   },
 })
